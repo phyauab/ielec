9963c2b8460dbb77f56cd654032afd31d480d0e8
phyauab <phyauab@connect.ust.hk>
2021-07-21 22:11:18 +0800

    add react route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import { React, useState } from "react";
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { GlobalStyle } from "./components/Theme/GlobalStyle";
 import Navbar from "./components/Navbar";
 import { lightTheme, darkTheme } from "./components/Theme/Theme";
+import {
+  AboutPage,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  ProductsPage,
+  SingleProductPage,
+} from "./pages";
 
 function App() {
   const [theme, setTheme] = useState("light");
   return (
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <GlobalStyle />
-      <div className="App">
+      <Router>
         <Navbar />
-      </div>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/products">
+            <ProductsPage />
+          </Route>
+          <Route path="/products/:id" children={<SingleProductPage />}>
+            <ProductsPage />
+          </Route>
+          <Route exact path="/about">
+            <AboutPage />
+          </Route>
+          <Route exact path="/cart">
+            <CartPage />
+          </Route>
+          <Route path="*">
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
